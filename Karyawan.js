283c75accb9b72a09081a918ecbf0602034ab6ac
egifadea <92985746+davidfathurrahman@users.noreply.github.com>
2022-11-02 12:33:00 +0700

    AplikasiDataKaryawan

---
+++ Karyawan.js
@@ -0,0 +1,23 @@
+const karyawan =[
+    {id:1, nama:'Candra Winangun ', jabatan:'Manager',
+    email: 'candra21@gmail.com', telp:'0858156728',
+    gaji:'100.000.000', foto:'https://placeimg.com/100/100/people'},
+
+    {id:2, nama:'Gandung pardiman', jabatan:'Sekertaris',
+    email: 'pardimangandung@gmail.com', telp:'08909876534',
+    gaji:'80.000.000', foto:'https://placeimg.com/100/100/arch'},
+
+    {id:3, nama:'Iswahyudi', jabatan:'Bendahara',
+    email: 'wahyudi35@gmail.com', telp:'08789865790',
+    gaji:'90.000.000', foto:'https://placeimg.com/100/100/nature'},
+
+    {id:4, nama:'Muhamad Deni', jabatan:'Anggota',
+    email: 'M.deni56@gmail.com', telp:'08567543627',
+    gaji:'50.000.000', foto:'https://placeimg.com/100/100/tech'},
+
+    {id:5, nama:'Farid', jabatan:'Anggota',
+    email: 'farid58@gmail.com', telp:'085768567354',
+    gaji:'50.000.000', foto:'https://placeimg.com/100/100/tech/sepia'}
+];
+
+export default karyawan;
